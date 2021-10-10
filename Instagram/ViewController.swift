25a350d74c578a705e79d2d88c8c0dbcc239c837
hayatemin <86780878+hayatemin@users.noreply.github.com>
2021-10-10 19:37:46 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 横山颯 on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
