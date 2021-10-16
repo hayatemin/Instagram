cbb3431927654398dc0861991e476a7bcfc0377c
hayatemin <86780878+hayatemin@users.noreply.github.com>
2021-10-16 17:48:00 +0900

    途中経過

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class PostViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
+    // 投稿ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handlePostButton(_ sender: Any) {
+    }
+    
+    // キャンセルボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleCancelButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
